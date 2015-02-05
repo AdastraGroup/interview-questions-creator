2aef6dd7808b9fc71713222bc17669f12dd6e07f
tlachy <tlachy@gmail.com>
2015-02-05 18:59:20 +0100

    Absolute minimum to start spring-boot web application:
    1, "spring-boot-starter-parent" dependency parent in pom.xml - version of this parent defines the version for all spring-boot dependencies as it itself has dependency parent "spring-boot-dependencies" containing <dependencyManagement> section with all possible spring-boot dependencies compatible one with another (also contains 3-party dependencies compatible with spring-boot dependencies)
    
    2, spring-boot-starter-web maven dependency in pom.xml (version defined in spring-boot-dependencies.pom)
    
    3, ApplicationConfig.java annotated with @EnableAutoConfiguration and simple main method

---
+++ src/main/java/com/adastragrp/iqc/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package com.adastragrp.iqc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class ApplicationConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConfig.class, args);
+    }
+
+}
