8644e3089ddd9d68e7688dec0a9a26096f0d625b
MartinTlachac <MartinTlachac@users.noreply.github.com>
2015-03-30 18:37:52 +0200

    - delete method not exported via rest, interview should never be deleted - 2 method exported via rest to find interview by state - two different styles but they do the same

--- src/main/java/com/adastragrp/iqc/repository/InterviewRepository.java
+++ src/main/java/com/adastragrp/iqc/repository/InterviewRepository.java
@@ -1,9 +1,27 @@
 package com.adastragrp.iqc.repository;
 
 import com.adastragrp.iqc.entity.Interview;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.Set;
 
 @RepositoryRestResource
 public interface InterviewRepository extends PagingAndSortingRepository<Interview, Long> {
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Long id);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Interview entity);
+
+    @Query("SELECT i FROM Interview i WHERE i.state = :state)")
+    public Set<Interview> find(@Param("state") Interview.State state);
+
+    public Set<Interview> findByState(@Param("state") Interview.State state);
 }
