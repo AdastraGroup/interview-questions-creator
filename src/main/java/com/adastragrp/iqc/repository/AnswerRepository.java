b3f99a934a089be7faaf0b550f01602da8eec362
MartinTlachac <MartinTlachac@users.noreply.github.com>
2015-03-30 18:04:51 +0200

    minor-change: forgotten annotation

--- src/main/java/com/adastragrp/iqc/repository/AnswerRepository.java
+++ src/main/java/com/adastragrp/iqc/repository/AnswerRepository.java
@@ -3,7 +3,9 @@ package com.adastragrp.iqc.repository;
 
 import com.adastragrp.iqc.entity.Answer;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface AnswerRepository extends PagingAndSortingRepository<Answer, Long> {
 
 }
