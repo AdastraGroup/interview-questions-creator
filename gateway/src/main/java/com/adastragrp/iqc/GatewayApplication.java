016660449cfd0e397416256c6f0a154f6a89db50
martin.tlachac <tlachy+adastra@gmail.com>
2015-05-29 17:29:23 +0200

    zuul reverse proxy working - just forwarding from port 8082 to 8080

---
+++ gateway/src/main/java/com/adastragrp/iqc/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.adastragrp.iqc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@Controller
+@EnableZuulProxy
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
