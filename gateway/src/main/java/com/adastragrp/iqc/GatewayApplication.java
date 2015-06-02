8e7f8f0693d2bbdee684d33bdb98aa935e7c27c4
martin.tlachac <tlachy+adastra@gmail.com>
2015-06-02 12:00:21 +0200

    basic auth implemented on Zuul proxy

--- gateway/src/main/java/com/adastragrp/iqc/GatewayApplication.java
+++ gateway/src/main/java/com/adastragrp/iqc/GatewayApplication.java
@@ -1,11 +1,18 @@
 package com.adastragrp.iqc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Controller;
+
 
 @Configuration
 @ComponentScan
@@ -16,5 +23,30 @@ public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Configuration
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+
+            auth.inMemoryAuthentication()
+                    .withUser("user").password("password").roles("USER");
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .httpBasic()
+                    .and()
+                    .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .csrf().disable();
+        }
+
+
     }
 }
